eb6ff9afdebf747630f96cca5f415c2cf8a03c97
Emin Devrim Fidan <devrimfidan@users.noreply.github.com>
2023-08-25 20:46:27 +0300

    cookie

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -2,4 +2,17 @@ import random
 
 lucky_number = random.randint(1,100)
 
-print(f'You will have a great day! Your Lucky Number is {lucky_number}')
+fortune_number = random.randint(1,3)
+
+fortune_text = ''
+
+if fortune_number == 1:
+    fortune_text = 'You will have a great Day!'
+
+if fortune_number == 2:
+    fortune_text = 'It will be tough... but worth it'
+
+if fortune_number == 3:
+    fortune_text = 'You will gt married this year!'
+
+print(f'{fortune_text}  Your Lucky Number is: {lucky_number}')
